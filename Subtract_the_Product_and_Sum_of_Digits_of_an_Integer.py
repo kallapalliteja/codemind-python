0838c8bdcf4a4f35d1824e000ba409514c7646c0
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-05-04 23:37:29 +0530

    '''n=int(input()) p=1 s=0 for i in str(n)):     s=s+int(i)     p=p*int(i) k=p-s print(k)''' a=int(input()) s=0 p=1 for i in str(a):     s=s+int(i)     p=p*int(i) k=p-s print(k)

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
@@ -0,0 +1,16 @@
+'''n=int(input())
+p=1
+s=0
+for i in str(n)):
+    s=s+int(i)
+    p=p*int(i)
+k=p-s
+print(k)'''
+a=int(input())
+s=0
+p=1
+for i in str(a):
+    s=s+int(i)
+    p=p*int(i)
+k=p-s
+print(k)
