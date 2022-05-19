fc8fb4fe688bff19682eef507b05d18b0cad5f0a
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-05-20 00:26:16 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         else:             return True n=int(input()) if prime(n):     k=str(n)     k=k[ : : -1]     if prime(int(k)):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

---
+++ circular_prime.py
@@ -0,0 +1,19 @@
+def prime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        else:
+            return True
+n=int(input())            
+if prime(n):
+    k=str(n)
+    k=k[ : : -1]
+    if prime(int(k)):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
+else:
+    print("not prime")
