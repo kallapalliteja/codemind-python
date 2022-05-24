fa092691eebc676d5f12e81200da7aa901bb7733
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-05-24 20:43:13 +0530

    def prime(n):     if n==0 or n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False                 break         else:             return True n=int(input()) l=len(str(n)) c=0 if prime(n):     for i in str(n):         if prime(int(i)):             c=c+1 if l==c:     print("Mega Prime") else:     print("Not Mega Prime")

---
+++ mega_prime.py
@@ -0,0 +1,23 @@
+def prime(n):
+    if n==0 or n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+                break
+        else:
+            return True
+n=int(input())  
+l=len(str(n))
+c=0
+if prime(n):
+    for i in str(n):
+        if prime(int(i)):
+            c=c+1
+if l==c:
+    print("Mega Prime")
+else:
+    print("Not Mega Prime")
+            
+            
