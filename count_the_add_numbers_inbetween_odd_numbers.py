05bdf7b4d2a137273d73b1848ac9dab4029450d5
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-09 11:45:11 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) c=0 od=0 for i in range(n-2):     for j in range(i,i+3):         if arr[j]%2!=0:             od=od+1     if od==3:         c=c+1     od=0 print(c)

---
+++ count_the_add_numbers_inbetween_odd_numbers.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+c=0
+od=0
+for i in range(n-2):
+    for j in range(i,i+3):
+        if arr[j]%2!=0:
+            od=od+1
+    if od==3:
+        c=c+1
+    od=0  
+print(c)    
+        
