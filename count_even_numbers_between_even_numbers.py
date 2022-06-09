8e0d4a7f92e8b2a32e81c93f2ed32e509d846c7e
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-09 11:46:32 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) c=0 ec=0 for i in range(n-2):     for j in range(i,i+3):         if arr[j]%2==0:             ec=ec+1     if ec==3:         c=c+1     ec=0 print(c)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+c=0
+ec=0
+for i in range(n-2):
+    for j in range(i,i+3):
+        if arr[j]%2==0:
+            ec=ec+1
+    if ec==3:
+        c=c+1
+    ec=0  
+print(c)    
+        
