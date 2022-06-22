19c81766febe3ea9ceaeba1bfa0ae298f5c9adcc
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-22 19:23:42 +0530

    s1=input() s2=input() #print(s1) #print(s2) s1=s1.lower() s2=s2.lower() s3=s1.split() s4=s2.split() k=[] for i in s4:     if i in s3:         k.append(i) print(*k)

---
+++ common_words.py
@@ -0,0 +1,15 @@
+s1=input()
+s2=input()
+#print(s1)
+#print(s2)
+s1=s1.lower()
+s2=s2.lower()
+s3=s1.split()
+s4=s2.split()
+k=[]
+for i in s4:
+    if i in s3:
+        k.append(i)
+print(*k)
+        
+        
