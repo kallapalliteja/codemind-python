cf43433d05dad7d6cddf70508ac4ea30f7346504
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-27 22:23:54 +0530

    n=int(input())# q=[] for i in str(n):     q.append(int(i)) for i in range(len(q)):     if(q[i]==6):         q[i]=9         break for i in range(len(q)):     print(q[i],end="")

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,11 +1,5 @@
 n=int(input())#
 q=[]
-'''for i in range(len(q)):
-    if q[i]==6:
-        q[i]=9
-        break
-for i in str(n):
-    print(i,end="")'''
 for i in str(n):
     q.append(int(i))
 for i in range(len(q)):
@@ -14,4 +8,3 @@ for i in range(len(q)):
         break
 for i in range(len(q)):
     print(q[i],end="")
-    
