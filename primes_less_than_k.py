948abea6fc2bc944edcfb7ab087cab87c0adad13
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-28 15:44:59 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         else:             return True n=int(input()) b=list(map(int,input().split())) k=int(input()) c=0 for i in range(n):     if prime(b[i]) and b[i]<=k:         c=c+1 print(c)

--- primes_less_than_k.py
+++ primes_less_than_k.py
@@ -1,14 +1,18 @@
 def prime(n):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return False
+    if n==1:
+        return False
     else:
-        return True
-n=int(input())        
-arr=list(map(int,input().strip().split()))
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        else:
+            return True
+n=int(input())   
+b=list(map(int,input().split()))
 k=int(input())
 c=0
 for i in range(n):
-    if arr[i]<=k and prime(arr[i]) and arr[i]!=1:
-            c=c+1
-print(c)            
+    if prime(b[i]) and b[i]<=k:
+        c=c+1
+print(c)        
+    
