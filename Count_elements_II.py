ca4d88aa200065f396163d3f12e946300d05a9e9
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-06-28 12:06:48 +0530

    a,b=map(int,input().split()) l=list(map(int,input().split())) m=list(map(int,input().split())) c=0 p1=set(l) p2=set(m) p1=list(p1) p2=list(p2) for i in range(len(p1)):     if p1[i] not in p2:         c=c+1 for i in range(len(p2)):     if p2[i] not in p1:         c=c+1 print(c)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,14 +1,16 @@
-n1,n2=map(int,input().split())
-arr1=list(map(int,input().split()))
-arr2=list(map(int,input().split()))
-k=[]
+a,b=map(int,input().split())
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
 c=0
-for i in range(n1):
-    if arr1[i] not in arr2 and  arr1[i] not in k:
-        k.append(arr1[i])
-for i in range(n2):
-    if arr2[i] not in arr1 and  arr2[i] not in k:
-        k.append(arr2[i])
-print(len(k))
-
-            
+p1=set(l)
+p2=set(m)
+p1=list(p1)
+p2=list(p2)
+for i in range(len(p1)):
+    if p1[i] not in p2:
+        c=c+1
+for i in range(len(p2)):
+    if p2[i] not in p1:
+        c=c+1
+print(c)        
+        
