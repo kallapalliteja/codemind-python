b34b291011669e8f769e21c4fe20d3b3110b5bd9
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-11-01 14:55:27 +0530

    n=int(input()) a=[] for i in range(n):     l=list(map(int,input().split()))     a.append(l) pd=0 sd=0 for i in range(n):     for j  in range(n):         if i==j:             pd=pd+a[i][j]         if i==(n-1)-j:             sd=sd+a[i][j] k1=str("Principal Diagonal:")+str(pd) k2=str("Secondary Diagonal:")+str(sd) print(k1) print(k2)

---
+++ Calculate_sums_of_diagonals_of_a_matrix.py
@@ -0,0 +1,19 @@
+n=int(input())
+a=[]
+for i in range(n):
+    l=list(map(int,input().split()))
+    a.append(l)
+pd=0
+sd=0
+for i in range(n):
+    for j  in range(n):
+        if i==j:
+            pd=pd+a[i][j]
+        if i==(n-1)-j:
+            sd=sd+a[i][j] 
+k1=str("Principal Diagonal:")+str(pd)  
+k2=str("Secondary Diagonal:")+str(sd)
+print(k1)
+print(k2)
+
+            
